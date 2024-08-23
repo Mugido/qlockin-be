55e6536e7feb2274f9a4a83be12ac49a4a01d0f5
Mugido <sirmugido707@gmail.com>
2024-08-23 13:29:22 +0100

    i updated view user details

--- src/main/java/com/decagosq022/qlockin/infrastructure/controller/UserController.java
+++ src/main/java/com/decagosq022/qlockin/infrastructure/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user")
+    @GetMapping("/")
     public ResponseEntity<?> getUserByEmail(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
